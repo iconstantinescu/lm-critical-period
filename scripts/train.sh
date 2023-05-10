8fb713cf0fa7699b20d046d0a4e712ea47bd5da2
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-10 20:54:40 +0200

    Update scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sbatch  --job-name="lm-training-hf" \
-        --output="./logs/train_hf.out" \
+model_arch=$1
+dataset=$2
+language=$3
+timestamp=$(date +%s)
+
+MODEL=${model_arch} DATASET=${dataset} LANGUAGE=${language} \
+sbatch  --job-name="lm-train_${model_arch}_${language}" \
+        --output="./logs/train_${model_arch}_${language}_${timestamp}.out" \
         scripts/train.euler
