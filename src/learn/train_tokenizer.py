976b7e06895f06aa8d85b3f9bab922cafc51232e
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-15 18:45:38 +0200

    Reimplement train_tokenizer.py

--- src/learn/train_tokenizer.py
+++ src/learn/train_tokenizer.py
@@ -1,10 +1,8 @@
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
-from tokenizers.processors import RobertaProcessing
+from tokenizers.implementations import ByteLevelBPETokenizer
+from transformers import AutoTokenizer
 
 import argparse
+import json
 
 sample = {
     "en": "Hello, y'all! How are you 😁 ?",
@@ -13,36 +11,35 @@ sample = {
 }
 
 
-def train_tokenizer(dataset, lang):
-    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-    tokenizer.pre_tokenizer = Whitespace()
+def train_tokenizer(model, dataset, lang):
+
+    tokenizer = ByteLevelBPETokenizer()
 
     files = [f"./data/{dataset}/{lang}/raw/{split}.txt" for split in ["test", "train", "validation"]]
 
-    trainer = BpeTrainer(vocab_size=32000,
-                         min_frequency=2,
-                         special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-    tokenizer.train(files, trainer)
+    tokenizer.train(files=files, vocab_size=32000, min_frequency=2,
+                    special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 
-    tokenizer.post_processor = RobertaProcessing(
-        ("[SEP]", tokenizer.token_to_id("[SEP]")),
-        ("[CLS]", tokenizer.token_to_id("[CLS]")),
-    )
-    tokenizer.enable_truncation(max_length=512)
+    tokenizer.save_model(f"./data/{dataset}/{lang}")
 
-    tokenizer.save(f"./data/{dataset}/{lang}/tokenizer-wiki.json")
+    # create a custom config.json file for AutoTokenizer
+    with open(f"./data/{dataset}/{lang}/config.json", 'w') as config:
+        json.dump({
+            'model_type': model
+        }, config)
 
     # Load the tokenizer and output a sample tokenization
-    test_tokenizer = Tokenizer.from_file(f"./data/{dataset}/{lang}/tokenizer-wiki.json")
-    output = test_tokenizer.encode(sample[lang])
-    print(output.tokens)
+    test_tokenizer = AutoTokenizer.from_pretrained(f"./data/{dataset}/{lang}")
+    output = test_tokenizer.encode_plus(sample[lang])
+    print(output.tokens())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str, help="the tokenizer configuration to use")
     parser.add_argument("dataset", type=str, help="the dataset to be processed")
     parser.add_argument("language", type=str, help="the language of the files")
     args = parser.parse_args()
 
-    print(f'\nTraining a custom tokenizer for data {args.dataset}_{args.language}\n')
-    train_tokenizer(args.dataset, args.language)
+    print(f'\nTraining a custom {args.model} tokenizer for data {args.dataset}_{args.language}\n')
+    train_tokenizer(args.model, args.dataset, args.language)
