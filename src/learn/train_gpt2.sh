d7feef83b9faadf68ad8671ced80398efede7c76
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-18 02:10:54 +0200

    Gpt training script

--- src/learn/train_gpt2.sh
+++ src/learn/train_gpt2.sh
@@ -1,12 +1,22 @@
+DATA_DIR=$1
+SEED=$2
+EXTRA_FLAGS=$3
+
 python ./src/learn/run_clm.py \
-    --model_name_or_path gpt2 \
-    --dataset_name wikitext \
-    --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --model_type gpt2 \
+    --tokenizer_name "${DATA_DIR}/gpt2_tokenizer" \
+    --cache_dir "${DATA_DIR}/cache" \
+    --train_file "${DATA_DIR}/raw/train.txt" \
+    --validation_file "${DATA_DIR}/raw/validation.txt" \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --block_size 512 \
     --do_train \
     --do_eval \
-    --output_dir ./checkpoints/gpt2-clm \
+    --output_dir "./checkpoints/gpt2-clm-${SEED}" \
     --overwrite_output_dir \
+    --learning_rate 1e-4 \
+    --num_train_epochs 5 \
     --fp16 \
-    --seed 42
+    --seed ${SEED} \
+    ${EXTRA_FLAGS}
