ea099352f3aa8e3b02715ae7909ecc883addd63d
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-30 00:23:38 +0200

    Update train config

--- src/learn/train_gpt2.sh
+++ src/learn/train_gpt2.sh
@@ -2,21 +2,26 @@ DATA_DIR=$1
 SEED=$2
 EXTRA_FLAGS=$3
 
-torchrun --nproc_per_node 8 ./src/learn/run_clm.py \
+torchrun --nproc_per_node 16 ./src/learn/run_clm.py \
     --model_type gpt2 \
     --tokenizer_name "${DATA_DIR}/gpt2_tokenizer" \
     --cache_dir "${DATA_DIR}/cache" \
     --train_file "${DATA_DIR}/raw/train.txt" \
     --validation_file "${DATA_DIR}/raw/validation.txt" \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --block_size 512 \
     --do_train \
     --do_eval \
+    --evaluation_strategy epoch \
     --output_dir "./checkpoints/gpt2-clm-${SEED}" \
     --overwrite_output_dir \
     --learning_rate 1e-4 \
-    --num_train_epochs 5 \
+    --num_train_epochs 12 \
+    --save_steps 0.1\
+    --streaming \
+    --low_cpu_mem_usage \
     --fp16 \
+    --ddp_timeout 7200 \
     --seed ${SEED} \
     ${EXTRA_FLAGS}
--- src/learn/train_roberta.sh
+++ src/learn/train_roberta.sh
@@ -2,23 +2,25 @@ DATA_DIR=$1
 SEED=$2
 EXTRA_FLAGS=$3
 
-torchrun --nproc_per_node 8 ./src/learn/run_mlm.py \
+torchrun --nproc_per_node 16 ./src/learn/run_mlm.py \
     --model_type roberta \
     --tokenizer_name "${DATA_DIR}/roberta_tokenizer" \
     --cache_dir "${DATA_DIR}/cache" \
     --train_file "${DATA_DIR}/raw/train.txt" \
     --validation_file "${DATA_DIR}/raw/validation.txt" \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 64 \
     --max_seq_length 510 \
     --do_train \
     --do_eval \
+    --evaluation_strategy epoch \
     --output_dir "./checkpoints/roberta-mlm-${SEED}" \
     --overwrite_output_dir \
     --learning_rate 5e-4 \
+    --mlm_probability 0.4 \
     --warmup_ratio 0.06 \
-    --max_steps  100000\
+    --num_train_epochs 12 \
     --save_steps 0.1\
     --streaming \
     --low_cpu_mem_usage \
