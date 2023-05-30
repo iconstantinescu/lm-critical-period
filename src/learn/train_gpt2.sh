aba464b5405b9638eb09321f6adc281dac1ad967
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-30 03:42:10 +0200

    Update train files

--- src/learn/train_gpt2.sh
+++ src/learn/train_gpt2.sh
@@ -2,12 +2,17 @@ DATA_DIR=$1
 SEED=$2
 EXTRA_FLAGS=$3
 
+DATE=$(date +%d/%m)
+
 torchrun --nproc_per_node 8 ./src/learn/run_clm.py \
     --model_type gpt2 \
     --tokenizer_name "${DATA_DIR}/gpt2_tokenizer" \
-    --cache_dir "${DATA_DIR}/cache" \
     --train_file "${DATA_DIR}/raw/train.txt" \
     --validation_file "${DATA_DIR}/raw/validation.txt" \
+    --cache_dir "${DATA_DIR}/cache" \
+    --run_name "gpt2-${DATA_DIR}-${SEED}-${DATE}" \
+    --output_dir "./checkpoints/gpt2-clm-${SEED}-${DATE}" \
+    --overwrite_output_dir \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 128 \
@@ -15,9 +20,7 @@ torchrun --nproc_per_node 8 ./src/learn/run_clm.py \
     --do_train \
     --do_eval \
     --evaluation_strategy "epoch" \
-    --output_dir "./checkpoints/gpt2-clm-${SEED}" \
     --save_strategy "epoch" \
-    --overwrite_output_dir \
     --learning_rate 1e-4 \
     --num_train_epochs 12 \
     --streaming \
--- src/learn/train_roberta.sh
+++ src/learn/train_roberta.sh
@@ -2,12 +2,17 @@ DATA_DIR=$1
 SEED=$2
 EXTRA_FLAGS=$3
 
+DATE=$(date +%d/%m)
+
 torchrun --nproc_per_node 8 ./src/learn/run_mlm.py \
     --model_type roberta \
     --tokenizer_name "${DATA_DIR}/roberta_tokenizer" \
-    --cache_dir "${DATA_DIR}/cache" \
     --train_file "${DATA_DIR}/raw/train.txt" \
     --validation_file "${DATA_DIR}/raw/validation.txt" \
+    --cache_dir "${DATA_DIR}/cache" \
+    --run_name "roberta-${DATA_DIR}-${SEED}-${DATE}" \
+    --output_dir "./checkpoints/roberta-mlm-${SEED}-${DATE}" \
+    --overwrite_output_dir \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 64 \
@@ -15,9 +20,7 @@ torchrun --nproc_per_node 8 ./src/learn/run_mlm.py \
     --do_train \
     --do_eval \
     --evaluation_strategy epoch \
-    --output_dir "./checkpoints/roberta-mlm-${SEED}" \
     --save_strategy "epoch" \
-    --overwrite_output_dir \
     --learning_rate 5e-4 \
     --mlm_probability 0.4 \
     --warmup_ratio 0.06 \
