063cd73bc3239931240102afca43e57657a1e854
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-05-30 03:03:13 +0200

    Reduce batch size

--- src/learn/train_roberta.sh
+++ src/learn/train_roberta.sh
@@ -8,8 +8,8 @@ torchrun --nproc_per_node 8 ./src/learn/run_mlm.py \
     --cache_dir "${DATA_DIR}/cache" \
     --train_file "${DATA_DIR}/raw/train.txt" \
     --validation_file "${DATA_DIR}/raw/validation.txt" \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 16 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 64 \
     --max_seq_length 510 \
     --do_train \
