9786f7fd339efaa4212ee677faab9ef40dd245ff
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-10-11 02:21:53 +0200

    Update loop.sh (fix tokenizers for gpt checkpoints)

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -9,10 +9,16 @@ LANG=$3
 for model_dir in ./checkpoints/${MODEL}-config2*-${LANG}*; do
     echo $model_dir
     for checkpoint in $model_dir/checkpoint-*; do
+      if [ "$MODEL" = gpt2 ]
+      then
+        echo -e "\nFixing tokenizer config for $checkpoint"
+        python3 ./src/learn/fix_tokenizer.py $checkpoint
+      fi
+
       CMD="./scripts/evaluate.sh blimp ${checkpoint#*checkpoints/} $MODEL_TYPE"
       echo $CMD
       eval $CMD
       sleep 480
     done
-    echo "\n"
+    echo -e "\n"
 done
