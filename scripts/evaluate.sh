c4c3baf9c63556e5021dbe51cc4c3d8c8c9ec760
Ionut Constantinescu <iconstantinescu100@gmail.com>
2023-10-15 18:08:46 +0200

    Scripts to evaluate fisher matrix

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,9 +3,10 @@
 task=$1
 model=$2
 model_type=$3
+lang=$4
 timestamp=$(date +%s)
 
-TASK=${task} MODEL=${model} MODEL_TYPE=${model_type} \
+TASK=${task} MODEL=${model} MODEL_TYPE=${model_type} LANG=${lang} \
 sbatch  --job-name="evaluate-${task}-${model}" \
         --output="./logs/evaluations/evaluate_${task}_${model}_${timestamp}.out" \
         scripts/evaluate.euler
---
+++ src/learn/eval_gpt2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EXTRA_FLAGS="$@"
+echo $EXTRA_FLAGS
+
+
+python3 ./src/learn/run_clm.py \
+    --model_name_or_path "./checkpoints/${MODEL_NAME}" \
+    --validation_file "${DATA_DIR}/raw/validation.txt" \
+    --cache_dir "${DATA_DIR}/cache" \
+    --run_name "eval_${MODEL_NAME}" \
+    --seed ${SEED} \
+    --report_to wandb \
+    --output_dir "./checkpoints/${MODEL_NAME}" \
+    --overwrite_output_dir \
+    --per_device_eval_batch_size 4 \
+    --eval_accumulation_steps 128 \
+    --block_size 512 \
+    --do_eval \
+    --low_cpu_mem_usage \
+    ${EXTRA_FLAGS}
