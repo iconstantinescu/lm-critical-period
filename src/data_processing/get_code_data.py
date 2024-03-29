65a165b66ef1f7a8659c066ee44ac2274e18957e
Ionut Constantinescu <iconstantinescu100@gmail.com>
2024-03-29 19:39:37 +0100

    Add script to get and preprocess code data

---
+++ src/data_processing/get_code_data.py
@@ -0,0 +1,38 @@
+import argparse
+
+from datasets import load_dataset
+import os
+import re
+
+
+def get_data(lang):
+    ds = load_dataset("bigcode/the-stack", streaming=True, data_dir=f"data/{lang}", split="train")
+
+    i = 0
+    code_lines = []
+    sample_size = 10**4
+
+    for sample in iter(ds.take(sample_size)):
+        code_string = sample['content']
+
+        code_string = re.sub(r"//(.?)+", "", code_string)  # remove  "//" comments
+        code_string = re.sub(r"/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/", "", code_string)  # remove "/* */" comments
+
+        code_lines.append(' '.join([line.strip() for line in code_string.splitlines() if line]))
+
+        if i % (sample_size / 10) == 0:
+            print(i // (sample_size // 10))
+
+        i += 1
+
+    out = open(f'./data/the_stack/{lang}.txt', "w")
+    out.write(os.linesep.join(code_lines))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("language", type=str, help="the coding language to be processed")
+    args = parser.parse_args()
+
+    print(f'\nGetting the code data for {args.language}\n')
+    get_data(args.language)
